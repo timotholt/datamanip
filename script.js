99a9368727b4a12a25becd53b417039bdafc6447
Timothy Otholt <timotholt@gmail.com>
2024-09-10 07:49:51 -0700

    Finished Part 1 - Math Problems

--- script.js
+++ script.js
@@ -42,7 +42,44 @@ console.log(isValid);
 const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4)
+  
+// == HOMEWORK PART 1: Math Problems START ================
+//
+console.log("Hello world from javascript file");
 
-  // =======================================================================
-  // =======================================================================
+//
+// Check if all numbers are divisible by 5. Cache the
+// result in a variable.
+  
+const isDivis5 = ((n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5)) === 0;
+console.log("is divisible by 5 = ", isDivis5);
+
+// Check if the first number is larger than the last. Cache
+// the result in a variable.
+
+const isFirstLargerThanLast = (n1 > n4);
+console.log("is the first number larger than last", isFirstLargerThanLast);
+
+// Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+// Multiply the result by the third number.
+// Find the remainder of dividing the result by the fourth number.
+
+const mathChainRemainder = ((n2 - n1) * n3) % n4;
+console.log("arithmethic chain remainder", mathChainRemainder);
+
+// Change the way that isOver25 calculates so that we do not need to
+// use the NOT operator (!) in other logic comparisons. Rename the
+// variable as appropriate.
+
+const isUnder26 = n1 < 26 || n2 < 26 || n3 < 26 || n4 < 26;
+const isValidV2 = isSum50 && isTwoOdd && isUnder26 && isUnique;
+console.log (`The four numbers are valid according to the provided criteria: ${isValidV2}.`)
+
+// == HOMEWORK PART 2 START ===============================
+
+// == HOMEWORK END ========================================
+
+console.log("Goodbye world from javascript file");
+
